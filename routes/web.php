143883999ae05cd7e0af196e61d1324105c16365
PappNiki20 <papp.nikoletta001@gmail.com>
2023-09-19 15:48:09 +0200

    pusholok

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,32 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/api/tasks', [TaskController::class, 'index']);
+/* GET -1 /api/task/1 */
+Route::get('/api/tasks/{id}', [TaskController::class, 'show']);
+/* POST
+/api/task */
+Route::post('/api/tasks', [TaskController::class, 'store']);
+/* PUT
+/api/task/1 */
+Route::put('/api/tasks/{id}', [TaskController::class, 'update']);
+/* DELETE
+/api/task/1 */
+Route::delete('/api/tasks/{id}'
+, [TaskController::class, 'destroy']);
+/* User API végpontok és feladatok
+GET - all /api/users */ Route::get('/api/users', [UserController::class, 'index']);
 
-//require __DIR__ . '/auth.php';
+Route::get('/task/new', [TaskController::class, 'newView']);
+Route::get('/task/edit/{id}', [TaskController::class, 'editView']);
+
+Route::get('/task/list', [TaskController::class, 'listView']);
+
+
+Route::get('/api/user', [UserController::class, 'index']);
+/* GET -1 /api/task/1 */
+Route::get('/api/user/{id}', [UserController::class, 'show']);
+/* POST
+/api/task */
+Route::post('/api/user', [UserController::class, 'store']);
+Route::get('/users/list', [UserController::class, 'listView']);
